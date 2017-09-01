2e8447e1b6ca584bece99b00567bd0b949d7b4f8
ksenia-mahilnaya <ksenia.vlady@gmail.com>
2017-09-01 19:46:16 +0300

    TASK-2 implemented getter method for retrieving contacts

--- app/controllers/PhoneBookController.js
+++ app/controllers/PhoneBookController.js
@@ -4,4 +4,5 @@ export default function PhoneBookController(PhoneBookService) {
     this.isPhoneNumbersVisible = true;
     this.addContact = PhoneBookService.addContact;
     this.removeContact = PhoneBookService.removeContact;
+    this.getAll = PhoneBookService.getAll;
 }
--- app/services/PhoneBookService.js
+++ app/services/PhoneBookService.js
@@ -5,53 +5,42 @@ export default function PhoneBookService() {
         {
             name: 'Ann',
             phoneNumber: '+375(33)4366445'
-        },
-        {
+        }, {
             name: 'Max',
             phoneNumber: '+375(29)4562387'
-        },
-        {
+        }, {
             name: 'Brom',
             phoneNumber: '+375(44)4562786'
-        },
-        {
+        }, {
             name: 'Dave',
             phoneNumber: '+375(22)3453455'
-        },
-        {
+        }, {
             name: 'Alonso',
             phoneNumber: '+375(33)6767674'
-        },
-        {
+        }, {
             name: 'Biber',
             phoneNumber: '+375(33)2347876'
-        },
-        {
+        }, {
             name: 'Joe',
             phoneNumber: '+375(25)4366445'
-        },
-        {
+        }, {
             name: 'Bucket',
             phoneNumber: '+375(25)7777654'
-        },
-        {
+        }, {
             name: 'Donald',
             phoneNumber: '+375(44)4366445'
-        },
-        {
+        }, {
             name: 'Aloha',
             phoneNumber: '+375(44)234567'
-        },
-        {
+        }, {
             name: 'Chip',
             phoneNumber: '+375(29)4562345'
-        }
-    ];
+        }];
 
     //localStorage.clear();
 
-    this.contacts = localStorage.getItem("contacts");
-    this.contacts = this.contacts ? JSON.parse(this.contacts) : contacts;
+    const contactsJSON = localStorage.getItem("contacts");
+    this.contacts = contactsJSON ? angular.fromJson(contactsJSON) : contacts;
 
     this.addContact = function(name, phoneNumber) {
         if (name && phoneNumber) {
